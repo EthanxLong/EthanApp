473fbaf50c758b2783fa7716ac14890c0c71ab0e
EthanxLong <exlong6@gmail.com>
2022-03-12 02:36:08 -0600

    commited

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const element = document.getElementById("ilybutton");
 element.addEventListener("click", iloveyouMore);
 
 function iloveyouMore() {
-    document.getElementById("hehe").innerHTML = "I know right 😩, ur so pretty ahh that smile 🥰 text me??";
-    document.getElementById("textme").innerHTML = '<a class="button" href="sms:18479093707&body=Heyyy lovey I miss yu">Text Me!</a>'
+    document.getElementById("hehe").innerHTML = "I know right 😩, u know you are.. text me??";
+    document.getElementById("textme").innerHTML = '<a class="button" href="sms:18479093707&body=I MISS YOU SOOOOO MUCH BEBE LUBBY">Tap here to send me a cute msg 😳</a>'
+}
+
+function getQuote() {
+    fetch('https://zenquotes.io/api/random/')
+}
+
+function getWeather( cityID ){
+    fetch('http://api.openweathermap.org/data/2.5/weather?id=' +cityID+ '&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
+    .then(function(resp) {return resp.json() })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function() {
+
+    });
+}
+
+window.onload = function() {
+    getWeather( 4259418 );
+    getQuote();
 }
