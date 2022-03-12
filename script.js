caf8c40bb8d76ef451e2ee8f53025446a26d9df5
EthanxLong <exlong6@gmail.com>
2022-03-12 01:57:06 -0600

    commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const element = document.getElementById("ilybutton");
 element.addEventListener("click", iloveyouMore);
 
 function iloveyouMore() {
-    document.getElementById("hehe").innerHTML = "So Perfect Dx, text me??";
+    document.getElementById("hehe").innerHTML = "I know right 😩, ur so pretty ahh that smile 🥰 text me??";
     document.getElementById("textme").innerHTML = '<a class="button" href="sms:18479093707&body=Heyyy lovey I miss yu">Text Me!</a>'
 }
