edc9c9138d578f98eeddc2731e37a4118fbcecd2
EthanxLong <exlong6@gmail.com>
2022-03-12 00:56:30 -0600

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("ilybutton");
+element.addEventListener("click", iloveyouMore);
+
+function iloveyouMore() {
+    document.getElementById("hehe").innerText = "So Perfect Dx";
+}
