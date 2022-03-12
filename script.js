c4b2043522c0338620dba573b3f17d9a8957b6e5
EthanxLong <exlong6@gmail.com>
2022-03-12 15:52:34 -0600

    init

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const element = document.getElementById("ilybutton");
 element.addEventListener("click", iloveyouMore);
-
 function iloveyouMore() {
     document.getElementById("hehe").innerHTML = "I LOVE YOU TOOOO! Why don't you text me??";
-    document.getElementById("textme").innerHTML = '<a class="button" href="sms:18479093707&body=I love you veeeerry much Ethan, this web app is awesome!">Tap here to send me a cute msg 😳</a>'
+    document.getElementById("textme").innerHTML = '<a class="button" href="sms:18479093707&body=I love you veeeerry much Ethan, this web app is awesome!">Wanna text??😳</a>'
     $("#ilybutton").remove();
 }
 
-function getQuote( random ){
-    fetch('https://quotes.rest' + random)
-    .then(function(resp) {return resp.json() })
+function getQuote(){
+    let N = 0 
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) { return response.json() })
     .then(function(data) {
-        console.log();
-    })
-    .catch(function() {
-
-    });
+        let date = new Date().toLocaleDateString();
+        if (localStorage.yourapp_date == date){
+            document.getElementById("quote").innerHTML = data[N].text;
+            document.getElementById("author").innerHTML = "-" + data[N].author;
+        }
+        if (localStorage.yourapp_date != date){
+            document.getElementById("quote").innerHTML = data[N + 1].text;
+            document.getElementById("author").innerHTML = data[N + 1].author;
+        }
+  });
 }
+
 
 function getWeather( cityID ){
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' +cityID+ '&units=imperial&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
@@ -24,10 +30,11 @@ function getWeather( cityID ){
     .then(function(data) {
         let desc = data.weather[0].description
         let temp = data.main.temp
+        temp = Math.trunc(temp)
         let picID = data.weather[0].icon
         let picURL = "http://openweathermap.org/img/wn/"+ picID +"@2x.png"
         if (temp < 50){
-            document.getElementById("temp").innerHTML = "Current Temp: " + temp + "°F 🥶🥶 WEAR A JACKET!";
+            document.getElementById("temp").innerHTML = "Current Temp: " + temp + "°F 🥶🥶";
         }
         document.getElementById("desc").innerHTML = "Description: " + desc;
         document.getElementById("icon").innerHTML = '<img src=' + 'http://openweathermap.org/img/wn/' + picID + '@2x.png>';
@@ -38,8 +45,9 @@ function getWeather( cityID ){
     
 }
 
+
 window.onload = function() {
     getWeather( 4259418 );
-    getQuote( '/qod' )
+    getQuote()
 }
 
