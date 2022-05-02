a545a46f1473dc189afe9a91a4a8f8cd1a6b2037
Ethan Long <ethanlong1227@icloud.com>
2022-05-02 17:36:20 -0400

    Refracted

--- script.js
+++ script.js
@@ -19,26 +19,26 @@ function getQuote(){
         let random = getRandomInt(100)
         let randomQuote = data.quotes[random].quote
         let randomAuthor = data.quotes[random].author
-
-        alert('"' + randomQuote + '"' + '\n' + '\n' + '- ' + randomAuthor)
+        document.getElementById("quote").innerHTML = (randomQuote)
+        document.getElementById("author").innerHTML = ("-" + randomAuthor)
+        $('#InspirationModal').modal()
     });
 }
 
 function getWeather(){
-    fetch("https://api.ipbase.com/v1/json/?apikey=c93fa3a0-a31c-11ec-90a4-6f848fd20c1a")
-    .then(function(response1) { return response1.json() })
-    .then(function(data1) {
-        let lat = data1.latitude
-        let long = data1.longitude
-        let country = data1.country_code
-        console.log(data1)
-
-        document.getElementById("location").innerHTML = data1.city + ', ' + data1.region_name
-
-
-        if (country == 'US') {
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=imperial&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
-        // http://api.openweathermap.org/data/2.5/weather?lat=42&lon=-88&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4
+    if (!navigator.geolocation) {
+        console.error(`Your browser doesn't support Geolocation`);
+    } else {navigator.geolocation.getCurrentPosition((position) => {
+        fetch('https://api.opencagedata.com/geocode/v1/json?key=cadf225b484c4d629d0ae4bf80c5493b&q='+ position.coords.latitude  + '%2C+' + position.coords.longitude + '&pretty=1&no_annotations=1')
+        .then(function(resp) {return resp.json() })
+        .then(function(data1) {
+            console.log(data1)
+            document.getElementById("location").innerHTML = data1.results[0].components.city + ', ' + data1.results[0].components.state_code
+            
+        
+    if (data1.results[0].components.country_code == "us") {
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=imperial&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
+        // http://api.openweathermap.org/data/2.5/weather?lat=39.8086&lon=-85.9873&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4
         .then(function(resp) {return resp.json() })
         .then(function(data) {
             let desc = data.weather[0].description
@@ -48,13 +48,14 @@ function getWeather(){
             document.getElementById("temp").innerHTML = temp + "°F";
             document.getElementById("desc").innerHTML = desc;
             document.getElementById("icon").innerHTML = '<img src=' + 'http://openweathermap.org/img/wn/' + picID + '@2x.png>';
+            
         })
         .catch(function() {
     
         });
 
     } else {
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4')
         // http://api.openweathermap.org/data/2.5/weather?lat=42&lon=-88&appid=a1b5b8dc03a7d9f84dc7e21d4d023ce4
         .then(function(resp) {return resp.json() })
         .then(function(data) {
@@ -70,8 +71,11 @@ function getWeather(){
         .catch(function() {
         });
     }
-    }); 
+    });
+});
 }
+} 
+
 
 function loadModal(){
     if( screen.width > 480 ) {     
@@ -79,8 +83,11 @@ function loadModal(){
     }
 }
 
+  
+
 window.onload = function() {
     getWeather();
     getFact();
     loadModal();
+    
 }
